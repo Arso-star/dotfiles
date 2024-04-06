a53d11cbf755a426ac88ea13c7609ed19117072b
Ariel Rodrigues <78938737+Arso-star@users.noreply.github.com>
2024-04-06 15:52:01 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 # [dotfiles - pacman]
-sudo pacman -S neovim lazygit zsh discord krita vlc lutris obs-studio steam steam-native-runtime wine wine-gecko wine-mono winetricks python-pip neofetch gimp jdk11-openjdk jre11-openjdk openjdk11-doc ntfs-3g jre11-openjdk mangohud goverlay blender
+sudo pacman -S neofetch noto-fonts noto-fonts-emoji noto-fonts-cjk discord krita vlc lutris obs-studio steam steam-native-runtime wine wine-gecko wine-mono winetricks python-pip gimp jdk11-openjdk jre11-openjdk openjdk11-doc ntfs-3g blender
